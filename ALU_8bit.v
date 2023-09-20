53a67b59f364a7a8dad54b5573f5137e462b4d8e
SuperSaish <129974287+SuperSaish@users.noreply.github.com>
2023-09-20 14:30:24 +0530

    Update ALU_8bit.v

--- ALU_8bit.v
+++ ALU_8bit.v
@@ -1,55 +1,56 @@
-module ALU_8bit(A,B,result,carry,zflag,select);
-  input [7:0]A,B;
-  input [3:0]select
-  output reg [15:0]result;
-  output reg carry,zflag;
+//ADD  = 3'b000
+//SUB  = 3'b001
+//MUL  = 3'b010
+//AND  = 3'b011
+//OR   = 3'b100
+//NAND = 3'b101
+//NOR  = 3'b110
+//XOR  = 3'b111
 
-  parameter [2:0]ADD = 3'b000,SUB = 3'b001,MUL = 3'b010,AND = 3'b011,OR = 3'b100,NAND = 3'b101,NOR = 3'b110,XOR = 3'b111;
+module ALU_8bit(
+  input [7:0] A, B,
+  input [2:0] select,
+  output reg [15:0] result,
+  output reg carry, zflag
+);
 
-  always@(A or B or select) begin
-    case(select) begin
-      default begin
-        result = 16'b0;
-        carry = 1'b0;
-        zflag = 1'b0;
-      end
-      ADD: begin
-        result = A + B;
-        carry = result[8];
-        zflag = (result == 16'b0);
-      end
-      SUB: begin
-        result = A - B;
-        carry = result[8];
-        zflag = (result == 16'b0);
-      end
-      MUL: begin
-        result = A*B;
-        zflag = (result == 16'b0);
-      end
-      AND: begin
-        result = A&B;
-        zflag = (result == 16'b0);
-      end
-      OR: begin
-        result = A|B;
-        zflag = (result == 16'b0);
-      end
-      NAND: begin
-        result = ~(A&B);
-        zflag = (result == 16'b0);
-      end
-      NOR: begin
-        result = ~(A|B);
-        zflag = (result == 16'b0);
-      end
-      XOR: begin
-        result = A^B;
-        zflag = (result == 16'b0);
-      end
-      endcase
+always @(A or B or select) begin
+  case (select) 
+    3'b000: begin 
+      result = A + B;
     end
-     
-      endmodule
-      
-        
+    3'b001: begin
+      result = A - B;
+    end
+    3'b010: begin
+      result = A * B;
+    end
+    3'b011: begin
+      result = A & B;
+    end
+    3'b100: begin
+      result = A | B;
+    end
+    3'b101: begin
+      result = ~(A & B);
+    end
+    3'b110: begin
+      result = ~(A | B);
+    end
+    3'b111: begin
+      result = A ^ B;
+    end
+    default: begin
+      result = 16'b0;
+    end
+  endcase
+  zflag = (result == 16'b0);
+  
+  if (select == 3'b000 || select == 3'b001) begin
+    carry = result[8];
+  end else begin
+    carry = 1'b0;
+  end
+end
+
+endmodule
